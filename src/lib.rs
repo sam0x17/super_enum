c465a486bb3b1d7558e413301e7013c36f00f3d1
Sam Johnson <sam@durosoft.com>
2023-08-04 19:25:54 -0400

    syntax that actually compiles

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,19 @@
+use proc_macro::TokenStream;
+use proc_macro2::TokenStream as TokenStream2;
+use quote::quote;
+use syn::Result;
 
+#[proc_macro_attribute]
+pub fn super_enum(attr: TokenStream, tokens: TokenStream) -> TokenStream {
+    match super_enum_internal(attr, tokens) {
+        Ok(tokens) => tokens.into(),
+        Err(err) => err.into_compile_error().into(),
+    }
+}
+
+fn super_enum_internal(
+    attr: impl Into<TokenStream2>,
+    tokens: impl Into<TokenStream2>,
+) -> Result<TokenStream2> {
+    Ok(quote!())
+}
--- tests/basic.rs
+++ tests/basic.rs
@@ -1,18 +1,18 @@
-// use super_enum::*;
+use super_enum::*;
 
-// pub enum Fizz {
-//     One,
-//     Two,
-//     Three
-// }
+pub enum Fizz {
+    One,
+    Two,
+    Three,
+}
 
-// #[super_enum]
-// pub enum MyEnum {
-//     None,
-//     @aggregate(some::path::Foo, some_mod::Bar, Fizz, Buzz),
-//     Other(i32, bool),
-//     my_field: u32,
-// }
+#[super_enum]
+#[aggregate(some::path::Foo, some_mod::Bar, Fizz, Buzz)]
+#[fields(my_field: u32)]
+pub enum MyEnum {
+    None,
+    Other(i32, bool),
+}
 
 // #[test]
 // fn test_super_enum() {
